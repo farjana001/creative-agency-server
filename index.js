1fe552e8b083a7c6b57cf4e88cb0dc3938204b82
Farjana Islam <nipa.farzana87@gmailcon>
2020-10-15 23:50:36 +0600

    fixing indentation

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const port = 5000;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const adminServiceCollection= client.db("creativeDatabase").collection("serviceData");
-    const orderCollection= client.db("creativeDatabase").collection("OrderData");
-    const reviewCollection= client.db("creativeDatabase").collection("reviews");
-    const adminEmailCollection= client.db("creativeDatabase").collection("makeAdmin");
-    const statusCollection= client.db("creativeDatabase").collection("status");
-    
+    const adminServiceCollection = client.db("creativeDatabase").collection("serviceData");
+    const orderCollection = client.db("creativeDatabase").collection("OrderData");
+    const reviewCollection = client.db("creativeDatabase").collection("reviews");
+    const adminEmailCollection = client.db("creativeDatabase").collection("makeAdmin");
+    const statusCollection = client.db("creativeDatabase").collection("status");
+
 
     // for image upload
     app.post('/addService', (req, res) => {
@@ -33,111 +33,102 @@ client.connect(err => {
         const title = req.body.title;
         const description = req.body.description;
         console.log(title, description, file);
+        
+        const newImg = req.files.file.data;
+        const encImg = newImg.toString('base64');
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
 
-        // const filePath = `${__dirname}/services/${file.name}`;
-        // file.mv(filePath, err => {
-        //     if(err){
-        //         console.log(err)
-        //         res.status(500).send({msg: 'Failed to upload image'})
-        //     }
-            const newImg = req.files.file.data;
-            const encImg = newImg.toString('base64');
-            var image = {
-                contentType: file.mimetype,
-                size: file.size,
-                img: Buffer.from(encImg, 'base64')
-            };
-
-            adminServiceCollection.insertOne({title, description, image})
+        adminServiceCollection.insertOne({ title, description, image })
             .then(result => {
-                // fs.remove(filePath, error => {
-                //     if(error){console.log(error)}
-                    res.send(result.insertedCount > 0 )
-                // });
-               
+                res.send(result.insertedCount > 0)
+
+
             })
-            // return res.send({name: file.name, path: `${file.name}`})
-        // })
+
     })
-    
+
 
     // showing serviceDat to ui
     app.get('/addService', (req, res) => {
         adminServiceCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })  
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
     // for sending order data
     app.post('/addOrder', (req, res) => {
         const order = req.body;
         orderCollection.insertOne(order)
-        .then(result => {
-            res.send(result.insertedCount > 0 )
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
     })
 
 
     // for showing all orders at admin service list page
     app.get('/orders', (req, res) => {
         orderCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
     // for single user order list 
     app.get('/singleUserOrder', (req, res) => {
-        orderCollection.find({email: req.query.email})
-        .toArray((err, documents) => {
-            res.send(documents)
-        })
+        orderCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
-        // for sending client review to server
-        app.post('/review', (req, res) => {
-            const order = req.body;
-            reviewCollection.insertOne(order)
-            .then(result => {
-                res.send(result.insertedCount > 0 )
-            })
-        })
-
-        // for showing review in ui
-        app.get('/review', (req, res) => {
-            reviewCollection.find({})
-            .toArray((err, documents) => {
-                res.send(documents)
-            })
-        })
-
-        // making admin api
-        app.post('/adminEmail', (req, res) => {
-            const email = req.body;
-            adminEmailCollection.insertOne(email)
+    // for sending client review to server
+    app.post('/review', (req, res) => {
+        const order = req.body;
+        reviewCollection.insertOne(order)
             .then(result => {
                 res.send(result.insertedCount > 0)
             })
-        })
+    })
 
-        // getting admin email collection
-        app.get('/adminEmail', (req, res) => {
-            adminEmailCollection.find({})
+    // for showing review in ui
+    app.get('/review', (req, res) => {
+        reviewCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents)
             })
-        })
+    })
 
-
-        // for status button
-        app.post('/status', (req, res) => {
-            const text = req.body;
-            statusCollection.insertOne(text)
+    // making admin api
+    app.post('/adminEmail', (req, res) => {
+        const email = req.body;
+        adminEmailCollection.insertOne(email)
             .then(result => {
                 res.send(result.insertedCount > 0)
             })
-        })
+    })
+
+    // getting admin email collection
+    app.get('/adminEmail', (req, res) => {
+        adminEmailCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+
+    // for status button
+    app.post('/status', (req, res) => {
+        const text = req.body;
+        statusCollection.insertOne(text)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
 
 });
 
